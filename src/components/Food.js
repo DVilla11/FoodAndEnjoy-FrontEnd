21bf4120ddb35b6be6419223ba3d45478b39b038
DVilla11 <david97villaj@gmail.com>
2022-06-19 23:45:25 +0200

    terminado

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,18 +1,128 @@
 import { useState } from 'react';
 import { ShowItemsCart } from './ShowItemsCart';
+import './Food.css'
 
 export const Food = ({platos, handleClickAddFood}) => {
-
-    const list = platos.map((plat) => { return (<li key={plat.id}>{plat.comida}<button onClick={() => handleClickAddFood(plat)}>Añadir</button></li>)})
 
     return (
         <div className="container_menu">
             <section>
-                <h2>PRIMEROS PLATOS</h2>
                 <article>
                     <ul>
-                        {list}
+                    <h2>ENTRANTES</h2>
+                        {
+                            platos.map((plat) => {
+                                if(plat.tipo === 'Entrante') 
+                                return (
+                                    <li className='food-line'  key={plat.comida_id}>
+                                        
+                                        <p>Nombre: </p>
+                                        <h4>{plat.nombreComida}</h4>
+
+                                        <p>Ingredientes: </p>
+                                        <h4>{plat.ingredientes}</h4>
+
+                                        <p>Precio: </p>
+                                        <h4>{plat.precio}€</h4>
+                                        <button onClick={() => handleClickAddFood(plat)}>Añadir</button>
+                                        
+                                        
+                                    </li>
+                                )
+                                else return null
+                            })
+                        }
+                    <h2>PRIMEROS PLATOS</h2>
+                        {
+                            platos.map((plat) => {
+                                if(plat.tipo === 'Primer plato') 
+                                return (
+                                    <li className='food-line'  key={plat.comida_id}>
+                                        
+                                        <p>Nombre: </p>
+                                        <h4>{plat.nombreComida}</h4>
+
+                                        <p>Ingredientes: </p>
+                                        <h4>{plat.ingredientes}</h4>
+
+                                        <p>Precio: </p>
+                                        <h4>{plat.precio}€</h4>
+                                        <button onClick={() => handleClickAddFood(plat)}>Añadir</button>
+                                        
+                                        
+                                    </li>
+                                )
+                                else return null
+                            })
+                        }
+                    <h2>SEGUNDOS PLATOS</h2>
+                        {
+                            platos.map((plat) => {
+                                if(plat.tipo === 'Segundo plato') 
+                                return (
+                                    <li className='food-line'  key={plat.comida_id}>
+                                        
+                                        <p>Nombre: </p>
+                                        <h4>{plat.nombreComida}</h4>
+
+                                        <p>Ingredientes: </p>
+                                        <h4>{plat.ingredientes}</h4>
+
+                                        <p>Precio: </p>
+                                        <h4>{plat.precio}€</h4>
+                                        <button onClick={() => handleClickAddFood(plat)}>Añadir</button>
+                                        
+                                        
+                                    </li>
+                                )
+                                else return null
+                            })
+                        }
+                    <h2>POSTRES</h2>
+                        {
+                            platos.map((plat) => {
+                                if(plat.tipo === 'Postre') 
+                                return (
+                                    <li className='food-line'  key={plat.comida_id}>
+                                        
+                                        <p>Nombre: </p>
+                                        <h4>{plat.nombreComida}</h4>
+
+                                        <p>Ingredientes: </p>
+                                        <h4>{plat.ingredientes}</h4>
+
+                                        <p>Precio: </p>
+                                        <h4>{plat.precio}€</h4>
+                                        <button onClick={() => handleClickAddFood(plat)}>Añadir</button>
+                                        
+                                        
+                                    </li>
+                                )
+                                else return null
+                            })
+                        }
+                    <h2>BEBIDAS</h2>
+                        {
+                            platos.map((plat) => {
+                                if(plat.tipo === 'Bebida') 
+                                return (
+                                    <li className='food-line'  key={plat.comida_id}>
+                                        
+                                        <p>Nombre: </p>
+                                        <h4>{plat.nombreComida}</h4>
+
+                                        <p>Precio: </p>
+                                        <h4>{plat.precio}€</h4>
+                                        <button onClick={() => handleClickAddFood(plat)}>Añadir</button>
+                                        
+                                        
+                                    </li>
+                                )
+                                else return null
+                            })
+                        }
                     </ul>
+                    
                 </article>
             </section>
         </div>
